bc82acbb4aaa417b04566a7ba7284f145f7c4dc0
Reyouf44 <riuof68@gmail.com>
2024-02-09 19:34:58 +0300

    insertionSort was added

--- Sorter.java
+++ Sorter.java
@@ -1,11 +1,10 @@
 
 public class Sorter {
-    public static void selectionSort(int[] arr){
-        for (int i = 0; i < arr.length - 1; i++)
-        {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int index = i;
-            for (int j = i + 1; j < arr.length; j++){
-                if (arr[j] < arr[index]){
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
                     index = j;//searching for lowest index
                 }
             }
@@ -15,19 +14,32 @@ public class Sorter {
         }
     }
 
-    public static void main(String a[]){
-        int[] arr1 = {9,14,3,2,43,11,58,22};
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {9, 14, 3, 2, 43, 11, 58, 22};
         System.out.println("Before Selection Sort");
-        for(int i:arr1){
-            System.out.print(i+" ");
+        for (int i : arr1) {
+            System.out.print(i + " ");
         }
         System.out.println();
 
         selectionSort(arr1);//sorting array using selection sort
 
         System.out.println("After Selection Sort");
-        for(int i:arr1){
-            System.out.print(i+" ");
+        for (int i : arr1) {
+            System.out.print(i + " ");
         }
     }
 }
